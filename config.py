2e6272a8281942cb3241ee473dc409a878182a54
Matthew <114261798+MattK158@users.noreply.github.com>
2024-12-18 22:24:30 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 API_WEATHER_URL="https://api.open-meteo.com/v1/forecast"
 GEO_SERVICE="https://ipinfo.io/loc"
 GEO_CODE="feros"
-OPEN_WEATHER_APIKEY="27eedd9e3f839130d3c83e4b0ed69202"
+OPEN_WEATHER_APIKEY=""
